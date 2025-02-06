1a88e7c9c6c85b6f9e74ddfc2145ed48efd7202c
Albert Fit <af@albert-fit.com>
2025-02-06 16:19:52 +0000

    added metadata in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css'
 
+export const metadata = { 
+    title: 'Property Pulse',
+    description: 'A real estate website',
+    keywords: 'real estate, property, homes, houses, rental', 
+    description: 'Find the perfect rental property.'
+}
 const MainLayout = ({children}) => {
     return ( <html>
         <body>
