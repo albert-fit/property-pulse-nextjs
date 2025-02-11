622b996ec6120c73cb6fae70152b26ff026c13c9
Albert Fit <af@albert-fit.com>
2025-02-11 16:07:53 +0000

    added api example

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,7 @@ import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage.jsx";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -12,17 +13,18 @@ const PropertyPage = async ({ params }) => {
       <PropertyHeaderImage image={property.images[0]} />
       <section>
         <div className="container m-auto py-6 px-6">
-          <a
-            href="/properties.html"
+          <Link
+            href="/properties"
             className="text-blue-500 hover:text-blue-600 flex items-center"
           >
             <FaArrowLeft className="mr-2"></FaArrowLeft> Back to Properties
-          </a>
+          </Link>
         </div>
       </section>
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+          <PropertyDetails property={property}/>
         </div>
       </section>
     </>
