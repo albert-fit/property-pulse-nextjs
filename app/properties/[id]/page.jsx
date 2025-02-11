52afd0f29686f64bb03eaf22b015e791a41353fe
Albert Fit <af@albert-fit.com>
2025-02-11 14:30:05 +0000

    started work on the property detail page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,32 @@
-const PropertyPage = () => {
-    return ( <div>Property page</div> );
-}
- 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage.jsx";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean();
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <a
+            href="/properties.html"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2"></FaArrowLeft> Back to Properties
+          </a>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default PropertyPage;
