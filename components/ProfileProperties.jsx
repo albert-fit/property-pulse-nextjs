09787fd5322d480a4c7fcba4c8d5e17ea6a70efa
Albert Fit <af@albert-fit.com>
2025-02-14 15:11:59 +0000

    added convertSerializableObject utility and closed the menu when clicking a link in nav bar

--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -2,13 +2,14 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProfileProperties = ({ properties: initialProperties }) => {
   const [properties, setProperties] = useState(initialProperties);
 
   return properties.map((property) => (
     <div className="mb-10" key={property._id}>
-    <a href="/property.html">
+    <Link href={`/properties/${property._id}`}>
       <Image
         className="h-32 w-full rounded-md object-cover"
         src={property.images[0]}
@@ -16,18 +17,18 @@ const ProfileProperties = ({ properties: initialProperties }) => {
         height={200}
         alt={property.name}
       />
-    </a>
+    </Link>
     <div className="mt-2">
       <p className="text-lg font-semibold">{property.name}</p>
       <p className="text-gray-600">Address: {property.location.street} {property.location.city} {property.location.state}</p>
     </div>
     <div className="mt-2">
-      <a
-        href="/add-property.html"
+      <Link
+        href="/add-property"
         className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
       >
         Edit
-      </a>
+      </Link>
       <button
         className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
         type="button"
