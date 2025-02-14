df46b21b7223743ca9201d2645414ef758ae7307
Albert Fit <af@albert-fit.com>
2025-02-14 13:45:30 +0000

    added property images in image component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage.jsx";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import PropertyDetails from "@/components/PropertyDetails";
+import PropertyImages from "@/components/PropertyImages";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -24,9 +25,10 @@ const PropertyPage = async ({ params }) => {
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"></div>
-          <PropertyDetails property={property}/>
+          <PropertyDetails property={property} />
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
