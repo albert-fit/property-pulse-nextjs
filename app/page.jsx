3a4cef60dceb04b4314978d49d909c22076a61e7
Albert Fit <af@albert-fit.com>
2025-06-18 17:53:07 +0100

    imported Mapbox and have primitive authentication mechanism

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 import connectDB from "@/config/database";
+
 const HomePage = ({ children }) => {
   connectDB();
   return (
